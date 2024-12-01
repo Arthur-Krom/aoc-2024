9a936425dd0ee9172da8c72bb0f222a9a47a1077
Arthur Krom <arthurkrom@hotmail.com>
2024-12-02 00:04:17 +0100

    day 1 part 1

--- day-1/src/main/java/nl/krom/arthur/Main.java
+++ day-1/src/main/java/nl/krom/arthur/Main.java
@@ -1,11 +1,45 @@
 package nl.krom.arthur;
 
-import reactor.core.publisher.Mono;
 
+import reactor.core.publisher.Flux;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Day 1: Historian Hysteria
+ */
 public class Main {
-    public static void main(String[] args) {
-        Mono.just("Hello world from day one!").subscribe(s -> {
-            System.out.println(s);
+    private static final Path INPUT_FILE_PATH = Path.of("day-1/src/main/resources/input.txt");
+    public static void main(String[] args) throws IOException {
+        var c = new BufferedReader(new FileReader(INPUT_FILE_PATH.toFile()));
+        var leftNrs = new ArrayList<Integer>();
+        var rightNrs = new ArrayList<Integer>();
+        AtomicInteger i = new AtomicInteger();
+
+        Flux.fromStream(c.lines()).subscribe(s ->{
+            i.getAndIncrement();
+
+            var leftStr = s.substring(0,s.indexOf(" ")).trim();
+            var rightStr = s.substring(s.indexOf(" ")).trim();
+            var leftNr = Integer.parseInt(leftStr);
+            var rightNr = Integer.parseInt(rightStr);
+
+            leftNrs.add(leftNr);
+            rightNrs.add(rightNr);
         });
+
+        leftNrs.sort(Comparator.comparingInt(a -> a));
+        rightNrs.sort(Comparator.comparingInt(a -> a));
+        Flux.zip(Flux.fromIterable(leftNrs), Flux.fromIterable(rightNrs), (left, right) -> Math.abs(left - right))
+                .reduce(Integer::sum).subscribe(r -> {
+            System.out.println("r: " +r);
+        });
+
     }
 }
