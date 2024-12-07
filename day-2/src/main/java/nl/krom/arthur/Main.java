fa7590bbb5afd7af0e3d72b4cef0a0b0ef207593
Arthur Krom <arthurkrom@hotmail.com>
2024-12-07 19:00:19 +0100

    day 2 part 1

--- day-2/src/main/java/nl/krom/arthur/Main.java
+++ day-2/src/main/java/nl/krom/arthur/Main.java
@@ -1,11 +1,71 @@
 package nl.krom.arthur;
 
-import reactor.core.publisher.Mono;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 public class Main {
-    public static void main(String[] args) {
-        Mono.just("Hello world from day two!").subscribe(s -> {
-            System.out.println(s);
-        });
+    private static final Path INPUT_FILE_PATH = Path.of("day-2/src/main/resources/input.txt");
+    private static final int MAX_DIFFERENCE = 3;
+    private static final int MIN_DIFFERENCE = 1;
+    public static void main(String[] args) throws IOException {
+        var bufferedReader = new BufferedReader(new FileReader(INPUT_FILE_PATH.toFile()));
+
+        Predicate<List<Integer>> isGradualIncrease = (a) -> {
+            BiPredicate<Integer, Integer> compareTwo = (b,c) -> b >= c - MAX_DIFFERENCE && b <= c - MIN_DIFFERENCE;
+
+            for (var i = 0; i < a.size(); i++) {
+                if(i + 1 == a.size()){
+                    break;
+                }
+
+                if(!compareTwo.test(a.get(i), a.get(i+1))){
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+        Predicate<List<Integer>> isGradualDecrease = (a) -> {
+            BiPredicate<Integer, Integer> compareTwo = (b,c) -> c + MAX_DIFFERENCE >= b && c <= b - MIN_DIFFERENCE;
+
+            for (var i = 0; i < a.size(); i++) {
+                if(i + 1 == a.size()){
+                    break;
+                }
+
+                if(!compareTwo.test(a.get(i), a.get(i+1))){
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+        Predicate<List<Integer>> isSafe = (a) -> isGradualIncrease.test(a) || isGradualDecrease.test(a);
+
+
+        var result = bufferedReader
+                .lines()
+                .reduce(0, ((currentTotal, next) -> {
+
+                    var nrs = Arrays
+                            .stream(next.split(" "))
+                            .mapToInt(Integer::parseInt)
+                            .boxed()
+                            .toList();
+
+
+                    return isSafe.test(nrs) ? currentTotal + 1 : currentTotal;
+                }), Integer::sum);
+
+        System.out.println("Result: " + result);
     }
+
 }
